85ae293e9db5546d34227476cf7c88a87dc44c09
Hercules Karkow <heco_karkow98@gmail.com>
2016-07-26 17:59:31 -0300

    adiciona jquery no gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var gulp = require('gulp');
 var gulpCopy = require('gulp-copy');
 
 gulp.task('default', function () {
-	gulp.src(['node_modules/bootstrap/dist/css/bootstrap.min.css', 'node_modules/bootstrap/dist/js/bootstrap.min.js'])
+	gulp.src([
+            'node_modules/bootstrap/dist/css/bootstrap.min.css', 
+            'node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/jquery/dist/jquery.min.js',
+        ])
 		.pipe(gulpCopy('www'));
 });
