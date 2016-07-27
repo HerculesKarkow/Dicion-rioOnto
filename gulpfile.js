e9963be8f5074c891782d701f98720b20729ef4d
Hercules Karkow <heco_karkow98@gmail.com>
2016-07-27 09:15:06 -0300

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ var gulpCopy = require('gulp-copy');
 
 gulp.task('default', function () {
 	gulp.src([
-            'node_modules/bootstrap/dist/css/bootstrap.min.css', 
-            'node_modules/bootstrap/dist/js/bootstrap.min.js',
-            'node_modules/jquery/dist/jquery.min.js',
+            'www/node_modules/bootstrap/dist/css/bootstrap.min.css', 
+            'www/node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'www/node_modules/jquery/dist/jquery.min.js',
         ])
 		.pipe(gulpCopy('www'));
 });
