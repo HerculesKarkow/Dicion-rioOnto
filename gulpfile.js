083107e74ea32b6ca31e7e55961aec2d3cc9c8e2
Hercules Karkow <heco_karkow98@gmail.com>
2016-07-27 15:07:19 -0300

    implementa lista com bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,14 @@ var gulpCopy = require('gulp-copy');
 
 gulp.task('default', function () {
 	gulp.src([
-            'www/node_modules/bootstrap/dist/css/bootstrap.min.css', 
-            'www/node_modules/bootstrap/dist/js/bootstrap.min.js',
-            'www/node_modules/jquery/dist/jquery.min.js',
+            'node_modules/bootstrap/dist/css/bootstrap.min.css', 
+            'node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/bootstrap/fonts/glyphicons-halflings-regular.eot',
+            'node_modules/bootstrap/fonts/glyphicons-halflings-regular.svg',
+            'node_modules/bootstrap/fonts/glyphicons-halflings-regular.ttf',
+            'node_modules/bootstrap/fonts/glyphicons-halflings-regular.woff',
+            'node_modules/bootstrap/fonts/glyphicons-halflings-regular.woff2',
         ])
 		.pipe(gulpCopy('www'));
 });
