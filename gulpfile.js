dcf0d19d6c859c5a934d8b5d63fc20d904a70412
Hercules Karkow <heco_karkow98@hotmail.com>
2016-07-28 10:38:18 -0300

    adiciona cordova.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,9 @@ gulp.task('default', function () {
             'node_modules/bootstrap/fonts/glyphicons-halflings-regular.ttf',
             'node_modules/bootstrap/fonts/glyphicons-halflings-regular.woff',
             'node_modules/bootstrap/fonts/glyphicons-halflings-regular.woff2',
+            'platforms/browser/www/cordova.js',
+            'platforms/ios/www/cordova.js'.,
+            'platforms/android/www/cordova.js'.,
         ])
 		.pipe(gulpCopy('www'));
 });
